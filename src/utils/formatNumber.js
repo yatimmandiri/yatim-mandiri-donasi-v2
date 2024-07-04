6c1f494d0572d2e96ef571fdddde3870e7294567
Yatim Mandiri <scrum@yatimmandiri.org>
2024-07-04 13:05:38 +0700

    Create formatNumber.js

---
+++ src/utils/formatNumber.js
@@ -0,0 +1,19 @@
+export const onlyNumber = (value) => {
+  return value.replace(/\D+/g, '');
+};
+
+export const formatRupiah = (angka, prefix) => {
+  var number_string = angka.replace(/[^,\d]/g, '').toString();
+  var split = number_string.split(',');
+  var sisa = split[0].length % 3;
+  var rupiah = split[0].substr(0, sisa);
+  var ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+  if (ribuan) {
+    var separator = sisa ? '.' : '';
+    rupiah += separator + ribuan.join('.');
+  }
+
+  rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+  return prefix == undefined ? rupiah : rupiah ? 'Rp. ' + rupiah : '';
+};

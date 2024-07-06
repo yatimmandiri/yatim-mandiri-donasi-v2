97b27700e31c01b4673c38e510a484288961054c
Yatim Mandiri <scrum@yatimmandiri.org>
2024-07-06 09:28:19 +0700

    Update axios.js

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -4,6 +4,7 @@ export const laravel = Axios.create({
   baseURL: '/',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
+    'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0',
   },
   withCredentials: true,
 });

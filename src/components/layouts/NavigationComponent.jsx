2b0513c29c2fafa469a096ea2d60c32702c77556
Yatim Mandiri <scrum@yatimmandiri.org>
2024-07-09 12:43:29 +0700

    Update NavigationComponent.jsx

--- src/components/layouts/NavigationComponent.jsx
+++ src/components/layouts/NavigationComponent.jsx
@@ -3,10 +3,10 @@
 import { UseApp } from '@/hooks/useApp';
 import classNames from 'classnames';
 import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 export const NavigationMenuComponent = () => {
+  const router = useRouter();
   const path = usePathname();
 
   const menus = [
@@ -15,41 +15,62 @@ export const NavigationMenuComponent = () => {
       name: 'Home',
       icons: '/assets/images/icon_home.svg',
       url: '/',
+      handler: () => {
+        router.refresh();
+        router.replace('/');
+      },
     },
     {
       id: 2,
       name: 'Riwayat',
       icons: '/assets/images/icon_donasi.svg',
       url: '/histori',
+      handler: () => {
+        router.refresh();
+        router.replace('/histori');
+      },
     },
     {
       id: 3,
       name: 'Kalkulator',
       icons: '/assets/images/icon_calculator.svg',
       url: '/kalkulator',
+      handler: () => {
+        router.refresh();
+        router.replace('/kalkulator');
+      },
     },
     {
       id: 4,
       name: 'Literasi',
       icons: '/assets/images/icon_literasi.svg',
       url: '/literasi',
+      handler: () => {
+        router.refresh();
+        router.replace('/literasi');
+      },
     },
     {
       id: 5,
       name: 'Account',
       icons: '/assets/images/icon_profile.svg',
       url: '/account',
+      handler: () => {
+        router.refresh();
+        router.replace('/account');
+      },
     },
   ];
 
   return (
     <ul className='flex justify-around items-center'>
       {menus.map((menu, i) => (
-        <Link
+        <li
           key={i}
+          onClick={menu.handler}
           href={menu.url}
           className={classNames(
-            'flex flex-col items-center justify-center p-1',
+            'flex flex-col items-center justify-center p-1 cursor-pointer',
             path == menu.url ? 'text-baseColor-500 font-semibold' : ''
           )}
         >
@@ -72,7 +93,7 @@ export const NavigationMenuComponent = () => {
             />
           </div>
           <span>{menu.name}</span>
-        </Link>
+        </li>
       ))}
     </ul>
   );

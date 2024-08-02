3c6ad6fad77e4c131ef2d0722f622096edcb3ccb
Yatim Mandiri <scrum@yatimmandiri.org>
2024-08-02 13:51:46 +0700

    Update page.jsx

--- src/app/(main)/payments/[notransaksi]/page.jsx
+++ src/app/(main)/payments/[notransaksi]/page.jsx
@@ -21,6 +21,21 @@ export default async function DetailTransaksiPage({ params }) {
       fixed: true,
     },
     pages: true,
+    pixel: {
+      purchase: {
+        content_ids: [donations.data[0].relationship.campaigns.id],
+        content_name: donations.data[0].relationship.campaigns.name,
+        contents: [
+          {
+            id: donations.data[0].no_transaksi,
+            quantity: donations.data[0].quantity,
+          },
+        ],
+        currency: 'IDR',
+        num_items: donations.data[0].quantity,
+        value: donations.data[0].totaldonasi,
+      },
+    },
   };
   return (
     <AppLayout context={contextValue}>

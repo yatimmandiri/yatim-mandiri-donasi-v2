13b266997c24b80100ed58836345e7c1d665c88d
Yatim Mandiri <scrum@yatimmandiri.org>
2024-08-06 09:05:57 +0700

    updated
    updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://next.yatimmandiri.org',
+  siteUrl: process.env.SITE_URL || 'https://donasi.yatimmandiri.org',
   generateRobotsTxt: true, // (optional)
   sourceDir: 'build',
   // ...other options

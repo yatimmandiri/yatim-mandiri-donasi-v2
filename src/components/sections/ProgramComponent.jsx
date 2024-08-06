a42272eb22b68dc6a4ad563f5ee990a6e8926eea
Yatim Mandiri <scrum@yatimmandiri.org>
2024-08-06 11:44:30 +0700

    Update ProgramComponent.jsx

--- src/components/sections/ProgramComponent.jsx
+++ src/components/sections/ProgramComponent.jsx
@@ -228,9 +228,9 @@ export const ProgramItemComponent = ({ item = [], priority = false }) => {
           className='object-center object-cover rounded'
         />
       </figure>
-      <div className='flex flex-col flex-1 justify-between space-y-4 h-36'>
+      <div className='flex flex-col flex-1 justify-between space-y-4 h-32'>
         <span className='font-semibold text-xs'>{item?.name}</span>
-        <span className='text-gray-700 line-clamp-3 flex-1'>
+        <span className='text-gray-700 line-clamp-2 flex-1'>
           {item.excerpt}
         </span>
         <div className='block'>

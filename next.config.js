bd9fdb037eb28efb00b96efdbc8c1b6d13dae382
Yatim Mandiri <scrum@yatimmandiri.org>
2024-08-06 09:01:39 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'donasi.yatimmandiri.org',
+      },
+      {
+        protocol: 'https',
         hostname: 'next.yatimmandiri.org',
       },
       {

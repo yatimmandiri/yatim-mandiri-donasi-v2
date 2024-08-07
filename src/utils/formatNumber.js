0f66446587b03b456ebd9218035338170218f3cc
Yatim Mandiri <scrum@yatimmandiri.org>
2024-08-07 10:11:12 +0700

    Update formatNumber.js

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -5,9 +5,12 @@ export const onlyNumber = (value) => {
 export const formatPhone = (value) => {
   const onlyNumberValue = onlyNumber(value);
 
-  return onlyNumberValue.substring(0, 2) == '08'
-    ? '628' + onlyNumberValue.substring(2)
-    : onlyNumberValue;
+  const str =
+    onlyNumberValue.substring(0, 2) == '08'
+      ? '628' + onlyNumberValue.substring(2)
+      : onlyNumberValue;
+
+  return str;
 };
 
 export const formatRupiah = (angka, prefix) => {
